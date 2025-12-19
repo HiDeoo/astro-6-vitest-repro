721f048753bfeb70e328eca1b768efe55cacfd6a
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-12-19 11:18:28 +0100

    Add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="vitest/config" />
+
+import { getViteConfig } from "astro/config";
+// import { defineConfig } from "vitest/config";
+
+export default getViteConfig({
+  test: {},
+});
+
+// export default defineConfig({
+//   test: {},
+// });
