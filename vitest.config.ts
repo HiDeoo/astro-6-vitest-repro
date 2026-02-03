ddd5edfe0531516de1b31d4d257216b337f8382c
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-02-03 16:35:08 +0100

    Update repro

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,9 @@
 /// <reference types="vitest/config" />
 
-import { getViteConfig } from "astro/config";
 import { defineConfig } from "vitest/config";
 
-export default getViteConfig({
-  test: {},
+export default defineConfig({
+  test: {
+    projects: ["tests/*"],
+  },
 });
-
-// export default defineConfig({
-//   test: {},
-// });
